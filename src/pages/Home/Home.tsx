0bafd70a3e5eee3dd8caeef926d30459cb12d445
Ihor Leonov <ihorleonow@gmail.com>
2023-11-12 18:09:52 +0200

    Update Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -110,23 +110,23 @@ const Home: FC = () => {
               backgroundColor: "#3C3E44",
               color: "#F5F5F5",
             },
-            ".css-19xm0h7-MuiButtonBase-root-MuiPaginationItem-root.Mui-selected": {
-              backgroundColor: "#F5F5F5",
-              color: "#202329",
-            },
             ".css-19xm0h7-MuiButtonBase-root-MuiPaginationItem-root.Mui-disabled": {
               backgroundColor: "#9E9E9E",
+              color: "#202329",
+            },
+            ".css-19xm0h7-MuiButtonBase-root-MuiPaginationItem-root.Mui-selected": {
+              backgroundColor: "#F5F5F5",
               color: "#202329",
             },
             "button:hover": {
               backgroundColor: "#FF9800",
               color: "#202329",
             },
-            "li:last-child button": {
+            "li:first-child button": {
               backgroundColor: "#F5F5F5",
               color: "#202329",
             },
-            "li:first-child button": {
+            "li:last-child button": {
               backgroundColor: "#F5F5F5",
               color: "#202329",
             },
